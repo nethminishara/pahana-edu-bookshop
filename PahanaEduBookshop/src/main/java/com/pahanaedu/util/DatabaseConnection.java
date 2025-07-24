35a615a997b88f22527f22c0866ceeca9b1bd3e5
Nethmi NIshara <amnnathapaththu@gmail.com>
2025-07-24 20:26:55 +0530

    git commit -m "Add singleton database connection utility"

---
+++ PahanaEduBookshop/src/main/java/com/pahanaedu/util/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package com.pahanaedu.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+    private static final String URL = "jdbc:mysql://localhost:3306/pahana_edu_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "your_password";
+    
+    private DatabaseConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException ex) {
+            throw new SQLException("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+    
+    public Connection getConnection() {
+        return connection;
+    }
+    
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+}
